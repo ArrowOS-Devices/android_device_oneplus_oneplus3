680bdd8703eacb5c7ac7530d6902d9a2b7e161f6
mukesh22584 <mks22584@gmail.com>
2018-04-24 23:15:00 +0530

    remove snap, add GoogleCamera & OnePlusGallery
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- device.mk
+++ device.mk
@@ -197,7 +197,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
     libcamera_parameters_shim \
     libcamera_shim \
     camera.device@1.0-impl \
@@ -205,7 +204,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.oneplus3 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    GoogleCamera \
+    OnePlusGallery
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
