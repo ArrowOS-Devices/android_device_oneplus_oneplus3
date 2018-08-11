8ab17e5e7968d996de5d7b4409b4d69fb3880e04
Ded_Boi <dhanush911gtr@gmail.com>
2018-08-11 09:15:45 +0000

    Remove OnePlusGallery
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- device.mk
+++ device.mk
@@ -205,8 +205,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service.oneplus3 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    GoogleCamera \
-    OnePlusGallery
+    GoogleCamera
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
