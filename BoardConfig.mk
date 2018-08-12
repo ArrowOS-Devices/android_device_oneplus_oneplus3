38f643152917f1d032270f48f6ed2e76ac70826a
LuK1337 <priv.luk@gmail.com>
2018-08-12 11:14:29 +0200

    op3: Disable our and QCOM sepolicy
    Change-Id: Id6d617ead095ef4a0d37e0d8ee1996cd7f0b10bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,9 +218,9 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_op3
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
