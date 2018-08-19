53c7e0fccb3b2868333460c5e9b435ff9cd8b11b
Artem Borisov <dedsa2002@gmail.com>
2018-08-19 19:56:24 +0000

    op3: Use common USB HAL
    Change-Id: Ic3a60d3d9c5b81c237386c97b35067232a665010

--- device.mk
+++ device.mk
@@ -445,6 +445,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
