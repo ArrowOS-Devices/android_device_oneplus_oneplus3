48112763b71b56655215abae7159b5e06a696547
Gaurav241 <gaurav.govinda2420@gmail.com>
2021-02-15 22:16:24 +0530

    op3: Update fingerprint to redfin february 2021

--- arrow_oneplus3.mk
+++ arrow_oneplus3.mk
@@ -37,6 +37,14 @@ DEVICE_MAINTAINER := Gaurav24
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
-
 TARGET_VENDOR := oneplus
+
+# Fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ1A.210205.004 7038034 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
